4dee2819f1def64983bfb0f91d91870412080f60
Chris A <superturtle41@gmail.com>
2020-08-23 14:55:26 -0400

    update mongodb config

--- dndbot/bot_config.py
+++ dndbot/bot_config.py
@@ -1,6 +1,6 @@
 import os
 
-prefix = ";"
+prefix = os.getenv('DISCORD_BOT_PREFIX', ';')
 environment = os.getenv("ENVIRONMENT", "testing")
 if environment == "testing":
     token = os.getenv("DISCORD_BOT_TOKEN_TESTING", "")
@@ -15,7 +15,7 @@ else:
     token = 'ERROR'
 testing = environment == "testing"
 
-mongo_host = 'mongodb:27017'
+mongo_host = os.getenv('MONGO_HOST', 'mongodb:27017')
 mongo_db = os.getenv('MONGO_DATABASE')
 mongo_user = os.getenv('MONGO_USERNAME')
 mongo_pass = os.getenv('MONGO_PASSWORD')
