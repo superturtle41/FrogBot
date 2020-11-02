28b9b7cd670f7cfcb6e44c11f28dad1839a84445
BuildTools <16567386+1drturtle@users.noreply.github.com>
2020-11-02 00:22:05 -0500

    add base dm command

--- cogs/dm_commands.py
+++ cogs/dm_commands.py
@@ -1,9 +1,27 @@
 from discord.ext import commands
+from .models.dm_objects import DMCategory
+from utils.functions import create_default_embed
+from utils.checks import is_owner
 
 
 class DMCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.group(name='dm', invoke_without_command=True)
+    @commands.check_any(commands.has_role('DM'), is_owner())
+    async def dm(self, ctx):
+        current_cat = await DMCategory.from_ctx(ctx)
+        embed = create_default_embed(ctx)
+        if current_cat is None:
+            embed.title = f'{ctx.author.display_name} does not have a DM Category!'
+            embed.description = f'Create a DM Category with {ctx.prefix}dm setup'
+        else:
+            embed.title = f'{ctx.author.display_name} checks their DM Category!'
+            embed.description = f'Number of Channels: {len(current_cat.channels)}'
+            for channel in current_cat:
+                embed.add_field(value=channel.channel.name, inline=True)
+        return await ctx.send(embed=embed)
 
 
 def setup(bot):
--- cogs/error_handeling.py
+++ cogs/error_handeling.py
@@ -42,12 +42,11 @@ class CommandErrorHandler(commands.Cog):
             if cog._get_overridden_method(cog.cog_command_error) is not None:
                 return
 
-        ignored = (commands.CommandNotFound, )
+        ignored = (commands.CommandNotFound,)
 
         # Allows us to check for original exceptions raised and sent to CommandInvokeError.
         # If nothing is found. We keep the exception passed to on_command_error.
         error = getattr(error, 'original', error)
-
 
         # Anything in ignored will return and prevent anything happening.
         if isinstance(error, ignored):
