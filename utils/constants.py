acd77688beec2edb74976e29ce96324ab8b4e41c
BuildTools <1drturtle@gmail.com>
2020-11-14 21:58:48 -0500

    fix online emoji

--- utils/constants.py
+++ utils/constants.py
@@ -4,7 +4,7 @@ BOT_MODS = ('DM', 'Dragonspeaker', 'Bot Admin')
 DMS = ('DM', 'Trial DM')
 
 STATUS_EMOJIS = {
-    'online': '<:online:777327499016929290>',
+    'online': '<:online:777327482054901781>',
     'idle': '<:idle:777327273249865758>',
     'dnd': '<:dnd:777327685566988318>',
     'offline': '<:offline:777327822963736576>'
